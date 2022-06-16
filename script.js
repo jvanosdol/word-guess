db71a8eee8b07be85dc12276fb56c6d98abef563
thegwanj <gwanjuchung@gmail.com>
2022-06-15 21:21:44 -0700

    adding in the content from timer

--- script.js
+++ script.js
@@ -18,4 +18,25 @@ document.addEventListener("keypress", function(event){
     userInput = event.key;
 
     checkGuess(userInput);
-})
+});
+
+
+var TimerInterval = 0;
+var oneSecond = 1000;
+var countdown = 10;
+var timeEl = document.querySelector( '#Timer' );
+
+function startTimer() {
+
+    TimerInterval = setInterval( function() {
+
+        countdown--;
+        timeEl.textContent = countdown;
+
+        if ( countdown ) {
+            clearInterval( TimerInterval );
+            timeEl.textContent = 'Times Up!';
+        }
+
+     }, oneSecond );
+}
